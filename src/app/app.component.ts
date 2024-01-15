4de6b5edf3d3ecd32779ae881d38dd66b6e294b0
surajmali1301 <143394824+surajmali1301@users.noreply.github.com>
2024-01-15 20:21:44 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ReactiveFormCrud';
+export class AppComponent implements OnInit {
+  title = 'ReactiveForm';
+  isUpdatebtn: boolean = false;
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.empForm.get('')?.valueChanges.subscribe((changeValue) => {
+      if (changeValue) {
+        this.salary?.setValidators(Validators.required);
+      } else {
+        this.salary?.clearValidators();
+      }
+      this.salary?.updateValueAndValidity();
+    });
+  }
+
+  empForm = this.fb.group({
+    id: [, Validators.required],
+    name: ['', Validators.required],
+    salary: [, Validators.required],
+  });
+
+  get id() {
+    return this.empForm.get('id');
+  }
+  get name() {
+    return this.empForm.get('name');
+  }
+  get salary() {
+    return this.empForm.get('salary');
+  }
+
+  edit(emp: any) {
+    this.isUpdatebtn = true;
+    this.empForm.setValue({
+      id: emp.id,
+      name: emp.name,
+      salary: emp.salary,
+    });
+  }
+  clearForm() {
+    this.empForm.reset();
+    this.isUpdatebtn = false;
+  }
+    addData(){
+  console.log(this.empForm.value);
+    }
 }
